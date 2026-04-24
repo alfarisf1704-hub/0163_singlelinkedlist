e3d0e72825174b6d6c835addd6b250ad41ab7bb2
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-24 15:33:46 +0700

    menambahkan While if dan node node

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -41,5 +41,18 @@ public:
 
         Node *previous = START;
         Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak dijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
 }
