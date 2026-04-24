a39f86bf26b8d3a43f50344bf9d785ba35c3e306
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-24 15:47:24 +0700

    menambahkan if else delete

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -80,5 +80,13 @@ public:
         Node *current, *previous;
         if (!Search(nim, &previous, &current))
             return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
     }
 }
